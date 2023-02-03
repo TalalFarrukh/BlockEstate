c2a814dbdb94227483368650bd2caddd2cc984b3
usman <you@example.com>
2023-02-02 23:37:40 -0800

    Prisma work

--- pages/api/registered.js
+++ pages/api/registered.js
@@ -1,14 +1,21 @@
 import conn from "utils/dbConnection"
+const { PrismaClient } = require('@prisma/client')
+const { Prisma } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 
 export default async function handler(req, res) {
 
     const { address } = req.body
     
-    const checkQuery = await conn.query(`Select is_registered from users where address = $1`, [address.toLowerCase()])
+    //const checkQuery = await conn.query(`Select is_registered from users where address = $1`, [address.toLowerCase()])
+    const checkQuery = await prisma.$queryRaw(Prisma.sql`SELECT is_registered FROM users WHERE address = ${address}`)//, [address.toLowerCase()])
+    //console.log(checkQuery);
+
     
-    if(checkQuery.rows.length > 0) {
+    if(checkQuery[0].length > 0) {
         res.json({
-            isRegistered: checkQuery.rows[0].is_registered
+            isRegistered: checkQuery[0].is_registered
         })
     }
     else {
